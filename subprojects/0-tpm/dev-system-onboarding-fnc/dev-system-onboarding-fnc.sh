c1416ccd6c84af9f3efcc1ecaaee72432f35a534
LambdaCalculus379 <viewtiful.icchan@gmail.com>
2017-01-29 19:54:36 -0500

    Modified script and added additional programming language choices; tested correct echo param

--- subprojects/0-tpm/dev-system-onboarding-fnc/dev-system-onboarding-fnc.sh
+++ subprojects/0-tpm/dev-system-onboarding-fnc/dev-system-onboarding-fnc.sh
@@ -10,10 +10,16 @@
 
 # Case switch is an elegant way of using 'if-else' functions
 
+SUPPORT_EMAIL='support@urbanspectra.com'
+ONBOARD_EMAIL='onboard@urbanspectra.com'
+
 echo "Options:
- a) Python 
+ a) Python
  b) Ruby
- c) Enter Unlisted Lang
+ c) node.js
+ d) Java
+ e) .NET
+ f) None of the above
 "
 read -p "What is your preferred programming language? " INPUT
 
@@ -22,12 +28,18 @@ read -p "What is your preferred programming language? " INPUT
 	      ;;
 	b)    DEVLANG='Ruby'
 	      ;;
-	?)    echo "Unsupported language!"
+	c)    DEVLANG='node.js'
+	      ;;
+	d)    DEVLANG='Java'
+	      ;;
+	e)    DEVLANG='.NET'
+	      ;;
+	?)    echo -e "Unsupported language! \nPlease email us at ${SUPPORT_EMAIL}. \nWe would like to support all languages!"
+	      exit
 	      ;;
     esac
 
 echo "Hello ${DEVLANG}!"    
-
 
 # ? Autotoggle two comments above with editor via script
 
